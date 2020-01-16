1e27d720fb5964113695b298bb2f49f871b2d8c1
Richard Hodges <hodges.r@gmail.com>
2020-01-16 11:57:58 +0100

    code formatting

--- src/config.hpp
+++ src/config.hpp
@@ -1,30 +1,27 @@
 #pragma once
 
 #include <boost/asio.hpp>
+
+// The following hack re-includes the boost/asio/buffer.hpp file within the namespace
+// boost::asio::v2
+// This will allow us to construct both v1 and v2 forms of the dynamic buffer specialisations
 #undef BOOST_ASIO_BUFFER_HPP
 namespace boost {
 namespace asio {
-namespace v2 {
+    namespace v2 {
+    namespace boost {
+        using ::boost::array;
+        namespace asio {
+        namespace detail {
+            using ::boost::asio::detail::throw_exception;
+            using ::boost::asio::detail::addressof;
+            using ::boost::asio::detail::is_buffer_sequence;
+            using ::boost::asio::detail::is_dynamic_buffer_v2;
+        }
+        }
+    }
 #define BOOST_ASIO_NO_DYNAMIC_BUFFER_V1
-
-namespace boost
-{
-    using ::boost::array;
-    namespace asio
-    {
-    namespace detail
-    {
-    using ::boost::asio::detail::throw_exception;
-    using ::boost::asio::detail::addressof;
-    using ::boost::asio::detail::is_buffer_sequence;
-    using ::boost::asio::detail::is_dynamic_buffer_v2;
-
-    }
-    }
-}
-
 #include <boost/asio/buffer.hpp>
-
 }
 }
 }
@@ -32,11 +29,8 @@ namespace boost
 #include <boost/beast.hpp>
 #include <boost/beast/ssl.hpp>
 
-namespace program
-{
-    using string_buffer_v1 = boost::asio::dynamic_string_buffer<char, std::char_traits<char>, std::allocator<char>>;
-    using string_buffer_v2 = boost::asio::v2::boost::asio::dynamic_string_buffer<char, std::char_traits<char>, std::allocator<char>>;
-    namespace net = boost::asio;
-    namespace beast = boost::beast;
-    namespace ssl = boost::asio::ssl;
+namespace program {
+namespace net = boost::asio;
+namespace beast = boost::beast;
+namespace ssl = boost::asio::ssl;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,35 @@
-#include <iostream>
-#include "explain.hpp"
 #include "config.hpp"
+
+#include "explain.hpp"
+#include <iostream>
 
 namespace program {
 
-    namespace websocket = beast::websocket;
-    using tcp = net::ip::tcp;
+using string_buffer_v1 =
+net::dynamic_string_buffer<char, std::char_traits<char>, std::allocator<char>>;
 
-    int
-    run()
-    {
+using string_buffer_v2 =
+net::v2::boost::asio::dynamic_string_buffer<char, std::char_traits<char>, std::allocator<char>>;
+
+int
+run()
+{
 //        std::string data;
 //        auto sv1 = string_buffer_v1(data);
 //        auto sv2 = string_buffer_v2(data);
 
-        std::cout << "string_buffer_v1 is dynamic_buffer_v1? " << std::boolalpha << net::is_dynamic_buffer_v1<string_buffer_v1>::value << '\n';
-        std::cout << "string_buffer_v1 is dynamic_buffer_v2? " << std::boolalpha << net::is_dynamic_buffer_v2<string_buffer_v1>::value << '\n';
+    std::cout << "string_buffer_v1 is dynamic_buffer_v1? " << std::boolalpha
+              << net::is_dynamic_buffer_v1<string_buffer_v1>::value << '\n';
+    std::cout << "string_buffer_v1 is dynamic_buffer_v2? " << std::boolalpha
+              << net::is_dynamic_buffer_v2<string_buffer_v1>::value << '\n';
 
-        std::cout << "string_buffer_v2 is dynamic_buffer_v1? " << std::boolalpha << net::is_dynamic_buffer_v1<string_buffer_v2>::value << '\n';
-        std::cout << "string_buffer_v2 is dynamic_buffer_v2? " << std::boolalpha << net::is_dynamic_buffer_v2<string_buffer_v2>::value << '\n';
+    std::cout << "string_buffer_v2 is dynamic_buffer_v1? " << std::boolalpha
+              << net::is_dynamic_buffer_v1<string_buffer_v2>::value << '\n';
+    std::cout << "string_buffer_v2 is dynamic_buffer_v2? " << std::boolalpha
+              << net::is_dynamic_buffer_v2<string_buffer_v2>::value << '\n';
 
-        return 0;
-    }
+    return 0;
+}
 }
 
 int
